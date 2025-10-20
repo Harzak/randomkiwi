674f746eac2c795edae319991bfb4c70dc6d7271
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2025-10-20 18:24:34 +0200

    Update Android theme, icons, and splash screen

--- randomkiwi/Platforms/Android/MainActivity.cs
+++ randomkiwi/Platforms/Android/MainActivity.cs
@@ -1,15 +1,25 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using AndroidGraphics = Android.Graphics;
 
 namespace randomkiwi.Platforms.Android;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/MainTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    private INavigationService _navigationService;
+    private readonly INavigationService _navigationService;
+
     public MainActivity()
     {
         _navigationService = IPlatformApplication.Current?.Services.GetRequiredService<INavigationService>() ?? throw new ArgumentException();
+    }
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        AndroidGraphics.Color color = new(GetColor(Resource.Color.colorPrimaryDark));
+        Window?.SetNavigationBarColor(color);
     }
 
     public async override void OnBackPressed()
